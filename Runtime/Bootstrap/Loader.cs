6bf84e68b6e8e5642a559eb2eb0d99c86025a779
Amit Reuveni <amitreuveni@gmail.com>
2026-01-14 14:34:38 +0200

    Fixed loader to not use the factory initialization where it is not needed, as well as using prefabentry list instead of dictionary as serializefield (internal is still dictionary)

--- Runtime/Bootstrap/Loader.cs
+++ Runtime/Bootstrap/Loader.cs
@@ -4,6 +4,7 @@ using UnityCoreKit.Runtime.Core.DefaultServices;
 using UnityCoreKit.Runtime.Core.Interfaces;
 using UnityCoreKit.Runtime.Core.Services;
 using UnityCoreKit.Runtime.Core.StubServices;
+using UnityCoreKit.Runtime.Core.Utils;
 using UnityCoreKit.Runtime.UserInteractions;
 using UnityCoreKit.Runtime.UserInteractions.Services;
 using UnityEngine;
@@ -19,7 +20,7 @@ namespace UnityCoreKit.Runtime.Bootstrap
     public class Loader : MonoBehaviour
     {
         // Add non-addressable prefabs here via Inspector if they need to be created via factory
-        [SerializeField] private Dictionary<string, GameObject> PrefabMap;
+        [SerializeField] private List<PrefabEntry> prefabEntries;
         
         // Set the base URL for your backend API here
         [SerializeField] private string baseUrl = "https://api.example.com";
@@ -27,9 +28,14 @@ namespace UnityCoreKit.Runtime.Bootstrap
         // Set your api endpoint mappings here for networking
         [SerializeField] private Dictionary<string, string> apiMap;
         
+        private Dictionary<string, GameObject> prefabMap;
+        
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
+            
+            InitializePrefabMap();
+
             SceneManager.sceneLoaded += OnSceneLoaded;
 
             RegisterServices();
@@ -38,47 +44,56 @@ namespace UnityCoreKit.Runtime.Bootstrap
             StartCoroutine(BootstrapCoroutine());
         }
 
+        private void InitializePrefabMap()
+        {
+            prefabMap = new Dictionary<string, GameObject>();
+            foreach (var entry in prefabEntries)
+            {
+                prefabMap[entry.key] = entry.prefab;
+            }
+        }
+
         /// <summary>
         /// Registers all core services in the service container.
         /// </summary>
         private void RegisterServices()
         {
             // Core event system
-            Register<IEventsManager>(() => new EventsManager());
-            Register<IEventListenerManager>(() => new EventListenerManager(Get<IEventsManager>()));
+            Register<IEventsManager>(new EventsManager());
+            Register<IEventListenerManager>(new EventListenerManager(Get<IEventsManager>()));
 
             // User Interactions Events Sub-system
             Register<IUserInteractions>(new UserInteractionsService(Get<IEventsManager>(), Get<IEventListenerManager>()));
             
-            // Object creation (Addressables + non-addressable prefabs)
+            //Object creation (Addressables + non-addressable prefabs)
             Register<IObjectFactory>(() =>
             {
                 var addressablesFactory = new AddressablesFactory();
-                var prefabFactory = new PrefabFactory(PrefabMap);
+                var prefabFactory = new PrefabFactory(prefabMap);
                 return new CompositeFactory(addressablesFactory, prefabFactory);
             });
 
             // Pooling depends on IObjectFactory
-            Register<IPoolManager>(() => new PoolManager(Get<IObjectFactory>()));
+            Register<IPoolManager>(new PoolManager(Get<IObjectFactory>()));
 
             // Persistence / save system
-            Register<ISaveSystem>(() => new JsonFileSaveSystem());
+            Register<ISaveSystem>(new JsonFileSaveSystem());
 
             // Audio & localization (stubs by default)
-            Register<IAudioManager>(() => new StubAudioManager());
-            Register<ILocalizationService>(() => new StubLocalizationService());
+            Register<IAudioManager>(new StubAudioManager());
+            Register<ILocalizationService>(new StubLocalizationService());
 
             // Scene loading & time service
-            Register<ISceneLoader>(() => new DefaultSceneLoader());
-            Register<ITimeService>(() => new DefaultTimeService());
+            Register<ISceneLoader>(new DefaultSceneLoader());
+            Register<ITimeService>(new DefaultTimeService());
             
             // Networking
-            Register<INetworkManager>(() => new DefaultNetworkManager(baseUrl, apiMap));
+            Register<INetworkManager>(new DefaultNetworkManager(baseUrl, apiMap));
 
             // Analytics / remote config / crash reporting (stubs for now)
-            Register<IAnalyticsService>(() => new StubAnalyticsService());
-            Register<IRemoteConfigService>(() => new StubRemoteConfigService());
-            Register<ICrashReportingService>(() => new StubCrashReportingService());
+            Register<IAnalyticsService>(new StubAnalyticsService());
+            Register<IRemoteConfigService>(new StubRemoteConfigService());
+            Register<ICrashReportingService>(new StubCrashReportingService());
         }
 
         /// <summary>
